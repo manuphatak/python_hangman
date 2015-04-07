48bc549e2a1ed983a22da58d7dbba2f5654bed51
Manu <bionikspoon@gmail.com>
2015-04-07 16:43:43 -0500

    hangman class and unit tests

--- hangman/hangman.py
+++ hangman/hangman.py
@@ -1,17 +1,73 @@
 # coding=utf-8
+import re
+
+
 class Hangman(object):
+    MAX_TURNS = 10
+
     def __init__(self, answer=None):
+        if not self.valid_answer(answer):
+            raise ValueError
+
         self.answer = answer.upper()
-        self.misses = []
-        self.remaining_turns = 10
-        self.status = '_' * len(answer)
+        self._misses = set()
+        self._hits = set()
+
+
+    @staticmethod
+    def valid_answer(text):
+        return re.compile('^[a-z]{1,16}$', re.I).search(str(text))
 
     def guess(self, letter):
-        miss = True
+        valid_guess = re.compile('^[a-z]$', re.I).search(str(letter))
 
-        if miss:
-            self.remaining_turns -= 1
+        if not valid_guess:
+            raise ValueError
 
-        self.misses.append(letter.upper())
+        is_miss = letter.upper() not in self.answer
+
+        if is_miss:
+            self.misses = letter
+        else:
+            self.hits = letter
 
         return self
+
+    @property
+    def misses(self):
+        return list(self._misses)
+
+    @misses.setter
+    def misses(self, value):
+        self._misses.add(value.upper())
+        if self.remaining_turns <= 0:
+            raise GameOver
+
+    @property
+    def hits(self):
+        return list(self._hits)
+
+    @hits.setter
+    def hits(self, value):
+        self._hits.add(value.upper())
+        if self._hits == set(self.answer):
+            raise GameWon
+
+    @property
+    def remaining_turns(self):
+        return self.MAX_TURNS - len(self.misses)
+
+    @property
+    def status(self):
+        result = [letter if letter in self.hits else '_' for letter in
+                  self.answer]
+
+        return ''.join(result)
+
+
+class GameWon(Exception):
+    pass
+
+
+class GameOver(Exception):
+    pass
--- test/test_hangman.py
+++ test/test_hangman.py
@@ -1,6 +1,6 @@
 # coding=utf-8
 import pytest
-from hangman.hangman import Hangman
+from hangman.hangman import Hangman, GameWon, GameOver
 
 
 @pytest.fixture
@@ -24,6 +24,28 @@ def test_new_game_returns_game_instance_with_status(game):
     assert game.status == '_______'
 
 
+def test_answer_validation_rules():
+    with pytest.raises(ValueError):
+        Hangman('1234567')
+
+    with pytest.raises(ValueError):
+        Hangman('hangman12')
+
+    with pytest.raises(ValueError):
+        Hangman('')
+
+    with pytest.raises(ValueError):
+        Hangman(1232145678995462313)
+
+    with pytest.raises(ValueError):
+        Hangman('a' * 100)
+    with pytest.raises(ValueError):
+        Hangman('a' * 17)
+
+    with pytest.raises(ValueError):
+        Hangman('hansdf-asdfsaf')
+
+
 def test_guess_miss_removes_1_turn(game):
     game.guess('e')
     assert game.remaining_turns == 9
@@ -34,4 +56,71 @@ def test_guess_miss_updates_misses(game):
     assert game.misses == ['E']
 
 
+def test_guess_miss_does_not_change_status(game):
+    expected = game.status
+    game.guess('e')
+    assert game.status == expected == '_______'
 
+
+def test_guess_validation_must_be_a_single_letter_number(game):
+    with pytest.raises(ValueError):
+        game.guess(1)
+
+    with pytest.raises(ValueError):
+        game.guess('EE')
+
+    with pytest.raises(ValueError):
+        game.guess('')
+
+
+def test_guess_miss_duplicate_is_ignored(game):
+    game.guess('e')
+    game.guess('e')
+    assert game.remaining_turns == 9
+
+
+def test_guess_hit_updates_status(game):
+    game.guess('a')
+    assert game.status == '_A___A_'
+
+    game.guess('H')
+    assert game.status == 'HA___A_'
+
+
+def test_guess_hit_leaves_remaining_turns_and_misses_untouched(game):
+    expected_misses = game.misses
+    expected_remaining_turns = game.remaining_turns
+
+    game.guess('a')
+
+    assert expected_misses == game.misses
+    assert expected_remaining_turns == game.remaining_turns
+
+
+def test_game_winning_guess(game):
+    game.guess('h')
+    game.guess('a')
+    game.guess('n')
+    game.guess('g')
+
+    with pytest.raises(GameWon):
+        game.guess('m')
+
+    assert game.status == 'HANGMAN'
+
+
+def test_game_losing_guess(game):
+    game.guess('b')
+    game.guess('c')
+    game.guess('d')
+    game.guess('e')
+    game.guess('f')
+    game.guess('i')
+    game.guess('j')
+    game.guess('k')
+    game.guess('l')
+
+    with pytest.raises(GameOver):
+        game.guess('o')
+    assert game.status == '_______'
+    assert game.remaining_turns == 0
