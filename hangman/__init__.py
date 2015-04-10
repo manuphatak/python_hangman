9955a3282f4ae75e1822eb6f028bee2e62e39895
Manu <bionikspoon@gmail.com>
2015-04-10 17:47:17 -0500

    cli entry point change

--- hangman/__init__.py
+++ hangman/__init__.py
@@ -6,4 +6,4 @@ from .dictionary import Dictionary
 from .hangman import Hangman, GameOver, GameWon
 from .presenter import Presenter
 from .commander import Commander
-from .app import cli
+# from .app import cli
