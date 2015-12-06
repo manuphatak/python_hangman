069a9af17d7cffe121c3fbcdeddd014999fe9df8
Manu Phatak <bionikspoon@gmail.com>
2015-12-05 23:09:35 -0600

    fix charts script

--- charts/charts.py
+++ charts/charts.py
@@ -1,89 +1,75 @@
-from contextlib import contextmanager
+import os
 import string
-from random import choice
-from cStringIO import StringIO
 import sys
+from random import choice
 
 from mock import patch
-from pycallgraph import (PyCallGraph, Config, GlobbingFilter,
-                         PyCallGraphException)
+from pycallgraph import (PyCallGraph, Config, GlobbingFilter, PyCallGraphException)
 from pycallgraph.output import GraphvizOutput
 
-from hangman import Commander
+from hangman import __version__ as version
+from hangman.controller import game_loop
 
 
-ITERATIONS = 100
-CHUNKS = 10
+class LocalConfig:
+    ITERATIONS = 100
+    CHUNKS = 10
+    ROOT = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
 
 
-@contextmanager
-def capture():
-    old_out, old_err = sys.stdout, sys.stderr
-    print "In..."
-    try:
-        out, err = StringIO(), StringIO()
-        sys.stdout, sys.stderr = out, err
-        yield (out, err)
-    finally:
-        sys.stdout, sys.stderr = old_out, old_err
-        out, err = out.getvalue(), err.getvalue()
-        print "Out..."
+def patch_getchar():
+    def random_letter():
+        return choice(string.ascii_uppercase)
+
+    return random_letter
 
 
-def random_letter():
-    return choice(string.ascii_uppercase)
-
-
-def new_game():
+def patch_confirm():
     while True:
-        for _ in xrange(CHUNKS):
+        for _ in list(range(LocalConfig.CHUNKS)):
             yield True
         yield False
 
 
-def main(tool='dot'):
+def graphiz_setup(tool='dot'):
     graphviz = GraphvizOutput()
-    graphviz.output_file = 'charts/basic-{}-{}.png'.format(ITERATIONS * CHUNKS,
-                                                           tool)
+    graphviz.output_file = os.path.join(LocalConfig.ROOT, 'charts', 'images',
+                                        'basic-{}-{}-v{}.png'.format(LocalConfig.ITERATIONS, tool, version))
     graphviz.tool = tool
+    return graphviz
 
+
+def config_setup():
     config = Config()
     config.trace_filter = GlobbingFilter(include=['hangman.*'])
-    with patch('click.getchar') as getchar_, patch('click.confirm') as confirm_:
-        getchar_.side_effect = random_letter
-        confirm_.side_effect = new_game()
+    return config
+
+
+def main(tool='dot'):
+    graphviz = graphiz_setup(tool)
+    config = config_setup()
+
+    # Patch click console prompts.  Patch click printing
+    with patch('click.getchar') as getchar, patch('click.confirm') as confirm, patch('click.echo'), patch(
+        'click.secho'), patch('click.clear'):
+        getchar.side_effect = patch_getchar()
+        confirm.side_effect = patch_confirm()
+
         with PyCallGraph(output=graphviz, config=config):
-            for i in xrange(ITERATIONS):
-                Commander.run()
+            print('Begin: Building <%r> call chart...' % tool)
+            for i in range(LocalConfig.ITERATIONS // LocalConfig.CHUNKS):
+                game_loop()
+                print('%4s trials completed' % ((i + 1) * LocalConfig.CHUNKS))
+            print('End: Done building <%r> call chart.' % tool)
 
 
 if __name__ == '__main__':
-    try:
-        main('dot')
-    except PyCallGraphException:
-        pass
+    # add project folder to PATH
+    sys.path.append(LocalConfig.ROOT)
 
-    # try:
-    #     main('neato')
-    # except PyCallGraphException:
-    #     pass
-    #
-    # try:
-    #     main('fdp')
-    # except PyCallGraphException:
-    #     pass
-    #
-    # try:
-    #     main('sfdp')
-    # except PyCallGraphException:
-    #     pass
-    #
-    # try:
-    #     main('twopi')
-    # except PyCallGraphException:
-    #     pass
-    #
-    # try:
-    #     main('circo')
-    # except PyCallGraphException:
-    #     pass
+    for tool in ['dot', 'neato', 'fdp', 'sfdp', 'twopi', 'circo']:
+        try:
+            main(tool)
+        except PyCallGraphException as e:
+            print('<%r> Failed.' % tool)
+            print(e)
